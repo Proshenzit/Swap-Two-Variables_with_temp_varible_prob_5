628569bcf02686d2e52c0e607186e2cdca35d79b
Proshenzit Roy <37993972+Proshenzit@users.noreply.github.com>
2024-01-07 21:58:58 -0800

    Add files via upload

---
+++ Swap Two Variables_with_temp_varible_prob_5.py
@@ -0,0 +1,7 @@
+a = int (input("Enter your first number: "))
+b = int (input("Enter your second  number: "))
+temp = a
+a = b
+b =temp
+print("your number first  number:",a)
+print("your number second number:",b)
